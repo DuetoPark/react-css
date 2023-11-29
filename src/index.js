f08d02c242e761a5a71f11702adc144854ebcbbb
DuetoPark <inhye.park.dev@gmail.com>
2023-11-29 14:17:29 +0900

    🎉 환경 설정

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Button1 from './Components/Button1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Button1 />
   </React.StrictMode>
 );
 
